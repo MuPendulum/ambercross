4b349c75719c440a0d8d3b493a32b2e215117684
Shimizu Hikaru <MuPendulum@users.noreply.github.com>
2022-10-08 01:57:00 -0400

    variables: update and disable hardening
    Enable graphite loop optimizations
    Disable hardening
    Add a few new default optimizations
    Default to GNU hash style which matches AmberELEC

--- scripts/variables.sh
+++ scripts/variables.sh
@@ -5,6 +5,10 @@ CORE_DIR="${CURRENT_DIR}/cores"
 OUT_DIR="${CURRENT_DIR}/release"
 BUILD_PLATFORM="rk3326"
 
+DISABLE_HARDENING_CFLAGS="-fno-stack-protector -fno-stack-clash-protection -fcf-protection=none -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=0"
+DISABLE_HARDENING_LDFLAGS="-Wl,-z,norelro,-z,lazy"
+ENABLE_GRAPHITE="-fgraphite-identity -floop-nest-optimize"
+
 # $CC -v
 export BUILD_TUPLE=x86_64-build_pc-linux-gnu
 export TARGET=aarch64-rg351-linux-gnu
@@ -22,7 +26,7 @@ export NM="${TARGET}-gcc-nm"
 export OBJCOPY="${TARGET}-objcopy"
 export OBJDUMP="${TARGET}-objdump"
 export STRIP="${TARGET}-strip"
-export CFLAGS="-mcpu=cortex-a35+crc+crypto --param l1-cache-size=32 --param l1-cache-line-size=64 --param l2-cache-size=256 -O3 -g -fipa-pta -fno-plt -flto=auto -ffat-lto-objects -fdevirtualize-at-ltrans"
+export CFLAGS="-mcpu=cortex-a35+crc+crypto --param l1-cache-size=32 --param l1-cache-line-size=64 --param l2-cache-size=256 -O3 -fomit-frame-pointer -fno-semantic-interposition -fipa-pta -fno-plt -flto=auto -ffat-lto-objects -fdevirtualize-at-ltrans $ENABLE_GRAPHITE $DISABLE_HARDENING_CFLAGS"
 export CXXFLAGS="$CFLAGS"
-export LDFLAGS="-Wl,-O1,--sort-common,--as-needed -flto=auto"
+export LDFLAGS="-Wl,-O1,--sort-common,--as-needed,--hash-style=gnu $DISABLE_HARDENING_LDFLAGS -flto=auto"
 #export MAKEFLAGS="-j1"
